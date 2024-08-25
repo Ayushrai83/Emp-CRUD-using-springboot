80c717e0dd4046be5f8959a1e16cfdd406c9047c
Ayushrai83 <gurukul4ayush@gmail.com>
2024-08-25 20:22:10 +0530

    Initial setup

---
+++ src/main/java/org/gurukul/em_project/EmProjectApplication.java
@@ -0,0 +1,13 @@
+package org.gurukul.em_project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EmProjectApplication.class, args);
+	}
+
+}
